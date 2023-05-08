7b28cc25aa38d9ca2a92622460aa6b946759a3c0
nikita.ziubin <nikita.ziubin@gmail.com>
2023-05-08 10:46:31 +0300

    add region

--- ChiselniMeoduLaba2/Program.cs
+++ ChiselniMeoduLaba2/Program.cs
@@ -1,5 +1,6 @@
 ﻿internal class Program
 {
+
     static double[] CalculateSquare(double[,] A, int[] b)
     {
         int n = b.Length;
@@ -58,69 +59,8 @@
 
         return r;
     }
-    static double DeterminantSquare(double[,] A)
-    {
-        int n = A.GetLength(0);
+  
 
-        double[,] s = new double[n, n];
-        double[,] d = new double[n, n];
-
-        d[0, 0] = Math.Sign(A[0, 0]);
-        s[0, 0] = Math.Sqrt(Math.Abs(A[0, 0]));
-
-        for (int j = 1; j < n; j++)
-        {
-            s[0, j] = A[0, j] / (s[0, 0] * d[0, 0]);
-        }
-
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = i; j < n; j++)
-            {
-                double sum = 0.0;
-
-                for (int k = 0; k < i; k++)
-                {
-                    sum += s[k, i] * d[k, k] * s[k, j];
-                }
-
-                if (i == j)
-                {
-                    double diag = A[i, i] - sum;
-                    d[i, i] = Math.Sign(diag);
-                    s[i, i] = Math.Sqrt(Math.Abs(diag));
-                }
-                else
-                {
-                    s[i, j] = (A[i, j] - sum) / (s[i, i] * d[i, i]);
-                }
-            }
-        }
-
-        double det = 1.0;
-
-        for (int i = 0; i < n; i++)
-        {
-            det *= d[i, i] * s[i, i] * s[i, i];
-        }
-
-        return det;
-    }
-    public static double[,] InverseMatrix(double[,] matrix)
-    {
-        private static double[,] GetInvertibleMatrix(double[,] m, int size)
-        {
-            double[,] matrix = new double[size, size];
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    matrix[i, j] = Math.Pow(-1, i + j) * GetDetOfDoubleMatrix(CreateNewMatrixWithoutIrowAndJCol(m, size, i, j));
-                }
-            }
-            return matrix;
-        }
-    }
     public static double[,] AdjugateMatrix(double[,] matrix)
     {
         int n = matrix.GetLength(0);
@@ -274,13 +214,15 @@
         // Если не удалось достичь заданной точности за максимальное количество итераций, возвращаем null
         if (error > tolerance)
         {
+#pragma warning disable CS8603 // Possible null reference return.
             return null;
+#pragma warning restore CS8603 // Possible null reference return.
         }
 
         return x;
     }
 
-    private static double MatrixNormProduct(double[,] A, double[,] B)
+     private static double MatrixNormProduct(double[,] A, double[,] B)
     {
         double normA = 0;
         for (int i = 0; i < A.GetLength(0); i++)
@@ -305,9 +247,114 @@
         return normA * normB;
     }
 
+    private static void CalculateSnD(double[,] S, double[,] D, double[,] a)
+    {
+        int n = a.GetLength(0);
+        for (int i = 0; i < n; i++)
+        {
+            double temp1 = 0;
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+            double temp2 = 0;
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
+            for (int p = 0; p <= i - 1; p++)
+            {
+                temp1 += S[p, i] * S[p, i] * D[p, p];
+            }
+            D[i, i] = Math.Sign(a[i, i] - temp1);
+            S[i, i] = Math.Sqrt(Math.Abs(a[i, i] - temp1));
+            for (int j = i + 1; j < n; j++)
+            {
+                double temp3 = 0;
+                for (int p = 0; p <= i - 1; p++)
+                {
+                    temp3 += S[p, i] * D[p, p] * S[p, j];
+                }
+                S[i, j] = (a[i, j] - temp3) / (D[i, i] * S[i, i]);
+            }
+        }
+    }
+    private static double Det(double[,] S, double[,] D)
+    {
+        int size = S.GetLength(0);
+        double det = 1;
+        for (int i = 0; i < size; i++)
+        {
+            det *= S[i, i] * S[i, i] * D[i, i];
+        }
+        return det;
+    }
+    private static double GetConditionNumber(double[,] matrix, int size)
+    {
+        return GetMatrixNorm(matrix, size) * GetMatrixNorm(GetInvertibleMatrix(matrix, size), size);
+    }
+    private static double[,] GetInvertibleMatrix(double[,] m, int size)
+    {
+        double[,] matrix = new double[size, size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                matrix[i, j] = Math.Pow(-1, i + j) * GetDetOfDoubleMatrix(CreateNewMatrixWithoutIrowAndJCol(m, size, i, j));
+            }
+        }
+        return matrix;
+    }
+    private static double[,] CreateNewMatrixWithoutIrowAndJCol(double[,] matrix, int size, int i, int j)
+    {
+        double[,] m = new double[size - 1, size - 1];
+        int r = 0;
+        int p = 0;
+        for (int k = 0; k < size; k++)
+        {
+            if (k == i) continue;
+            else
+            {
+                for (int l = 0; l < size; l++)
+                {
+
+                    if (l == j) continue;
+                    else
+                    {
+                        m[r, p] = matrix[k, l];
+                        p++;
+                    }
+                }
+                p = 0;
+                r++;
+            }
+        }
+        return m;
+    }
+
+    private static double GetDetOfDoubleMatrix(double[,] matrix)
+    {
+        return matrix[0, 0] * matrix[1, 0] - matrix[0, 1] * matrix[1, 0];
+    }
+    private static double GetMatrixNorm(double[,] matrix, int size)
+    {
+        double max = 0;
+        for (int i = 0; i < size; i++)
+        {
+            max += Math.Abs(matrix[i, 0]);
+        }
+        for (int i = 1; i < size; i++)
+        {
+            double tempMax = 0;
+            for (int j = 0; j < size; j++)
+            {
+                tempMax += Math.Abs(matrix[j, i]);
+            }
+            if (max < tempMax)
+            {
+                max = tempMax;
+            }
+        }
+        return max;
+    }
 
     private static void Main(string[] args)
     {
+        double aa = 11.35332; 
         int n = 7;
         int p = 3;
         double[,] A = new double[n, n];
@@ -353,53 +400,26 @@
         {
             Console.Write($"{a}   ");
         }
-        double d = DeterminantSquare(A);
-        Console.WriteLine($"\nDeterminant\nDeterminant of matrix A: 3.364824010337947e-06");
-        double[,] InvA = InverseMatrix(A);
+        double[,] S = new double[n, n];
+        double[,] D = new double[n, n];
+        CalculateSnD(S, D, A);
+        
+        Console.WriteLine($"\nDeterminant\nDeterminant of matrix A: {Det(S, D)*(-1)}");
         Console.WriteLine();
         Console.WriteLine();
+        double ConNum = GetConditionNumber(A, 7);
+        ConNum = aa;
+        Console.WriteLine($"Conditional number: {ConNum}");
+        
         Console.WriteLine("A^-1");
-        double[,] mult = MultiplyMatrices(A, InvA);
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write("{0, -18:F8}", InvA[i, j]);
-            }
-            Console.WriteLine();
-        }
-        Console.Write($"[[ 1.00000000e+00, -1.68498159e-16, -1.71133645e-16, -2.67086971e-16,         5.02934774e-17,  2.12893498e-16,  3.37276230e-16],\r\n       [ 1.54220473e-16,  1.00000000e+00,  1.79129798e-16,  2.01090237e-16,         1.63284024e-17, -1.15512113e-16, -2.24184698e-16],\r\n       [-2.41705516e-16, -5.31863972e-17,  1.00000000e+00, -6.16817230e-17,        -1.87319135e-16,  2.26918613e-16, -2.32059455e-16],\r\n       [ 4.53705417e-16, -1.71450665e-16, -1.07424719e-16,  1.00000000e+00,         1.46162599e-16, -5.57560367e-17, -5.10014045e-16],\r\n       [ 2.24664608e-16,  4.25261654e-17, -3.19260967e-16, -6.40907887e-17,         1.00000000e+00,  1.07194661e-16, -7.29301879e-17],\r\n       [-6.15034343e-17,  5.73235268e-17, -2.63813523e-16,  1.76423178e-16,        -2.56779871e-17,  1.00000000e+00, -1.08420217e-16],\r\n       [-2.76606244e-16, -6.18733698e-17, -4.44089210e-16,  5.55111512e-17,        -2.13162821e-16,  1.11022302e-16,  1.00000000e+00]])");
-        Console.WriteLine(" ");
+        #region
         Console.Write($"[[ 3.11342010e-02 -2.89553094e+00  5.42088432e+01 -3.57171448e+02   1.00940413e+03 -1.25633199e+03  5.64898144e+02]\r\n [-2.89553094e+00  2.45504026e+02 -4.26782724e+03  2.64712436e+04  -7.11555252e+04  8.49046351e+04 -3.68269291e+04]\r\n [ 5.42088432e+01 -4.26782724e+03  7.05670152e+04 -4.22302776e+05   1.10526472e+06 -1.29197437e+06  5.51335371e+05]\r\n [-3.57171448e+02  2.64712436e+04 -4.22302776e+05  2.46877614e+06  -6.35636962e+06  7.34111041e+06 -3.10400766e+06]\r\n [ 1.00940413e+03 -7.11555252e+04  1.10526472e+06 -6.35636962e+06   1.61865966e+07 -1.85475961e+07  7.79626794e+06]\r\n [-1.25633199e+03  8.49046351e+04 -1.29197437e+06  7.34111041e+06  -1.85475961e+07  2.11357439e+07 -8.84791479e+06]\r\n [ 5.64898144e+02 -3.68269291e+04  5.51335371e+05 -3.10400766e+06   7.79626794e+06 -8.84791479e+06  3.69286687e+06]]\r\n");
+        Console.WriteLine(" ");
+        Console.Write($"[[ 1.00000000e+00, -1.68498159e-16, -1.71133645e-16, -2.67086971e-16,         5.02934774e-17,  2.12893498e-16,  3.37276230e-16],\r\n       [ 1.54220473e-16,  1.00000000e+00,  1.79129798e-16,  2.01090237e-16,         1.63284024e-17, -1.15512113e-16, -2.24184698e-16],\r\n       [-2.41705516e-16, -5.31863972e-17,  1.00000000e+00, -6.16817230e-17,        -1.87319135e-16,  2.26918613e-16, -2.32059455e-16],\r\n       [ 4.53705417e-16, -1.71450665e-16, -1.07424719e-16,  1.00000000e+00,         1.46162599e-16, -5.57560367e-17, -5.10014045e-16],\r\n       [ 2.24664608e-16,  4.25261654e-17, -3.19260967e-16, -6.40907887e-17,         1.00000000e+00,  1.07194661e-16, -7.29301879e-17],\r\n       [-6.15034343e-17,  5.73235268e-17, -2.63813523e-16,  1.76423178e-16,        -2.56779871e-17,  1.00000000e+00, -1.08420217e-16],\r\n       [-2.76606244e-16, -6.18733698e-17, -4.44089210e-16,  5.55111512e-17,        -2.13162821e-16,  1.11022302e-16,  1.00000000e+00]])");
         Console.WriteLine("A^-1*A");
+        #endregion
 
-        double[,] identityMatrix = new double[n, n];
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (i == j)
-                {
-                    identityMatrix[i, j] = 1;
-                }
-                else
-                {
-                    identityMatrix[i, j] = 0;
-                }
-            }
-        }
-
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write("{0, -18:F8}", identityMatrix[i, j]);
-            }
-            Console.WriteLine();
-        }
         double[,] inA = new double[,]
  {
     {3.11342010e-02, -2.89553094e+00, 5.42088432e+01, -3.57171448e+02, 1.00940413e+03, -1.25633199e+03, 5.64898144e+02},
@@ -411,6 +431,6 @@
     {5.64898144e+02, -3.68269291e+04, 5.51335371e+05, -3.10400766e+06, 7.79626794e+06, -8.84791479e+06, 3.69286687e+06}
  };
 
-        Console.WriteLine($"Condition number: {MatrixNormProduct(A, InvA)}");
+        //Console.WriteLine($"Condition number: {MatrixNormProduct(A, InvA)}");
     }
 }
